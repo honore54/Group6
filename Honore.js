b85cd45903a66de76f03f980b3011e1aea9982a3
Honore <honoremugisha54@gmail.com>
2025-03-26 20:14:24 +0200

    `Refactor: Extracted getCharacterAt function and added test cases`

--- Honore.js
+++ Honore.js
@@ -1,13 +1,15 @@
 const str = "Hello, World!";
 
+function getCharacterAt(index) {
+    return str.at(index);
+}
 
-console.log(str.at(0));  
-console.log(str.at(7));  
+console.log(getCharacterAt(0));  
+console.log(getCharacterAt(7));  
 
+console.log(getCharacterAt(-1)); 
+console.log(getCharacterAt(-6)); 
 
-console.log(str.at(-1)); 
-console.log(str.at(-6)); 
+console.log(getCharacterAt(20)); 
+console.log(getCharacterAt(-20));
 
-
-console.log(str.at(20)); 
-console.log(str.at(-20)); 
