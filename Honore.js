8758deec27f680d9ae3594f553aa13fcb9eb1c08
Honore <honoremugisha54@gmail.com>
2025-03-26 11:16:12 +0200

    .at string method functionality

---
+++ Honore.js
@@ -0,0 +1,13 @@
+const str = "Hello, World!";
+
+
+console.log(str.at(0));  
+console.log(str.at(7));  
+
+
+console.log(str.at(-1)); 
+console.log(str.at(-6)); 
+
+
+console.log(str.at(20)); 
+console.log(str.at(-20)); 
